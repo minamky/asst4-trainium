2c7277d270840f49e25a24a9c99252e12e0bd55d
Mina Ky <128256105+minamky@users.noreply.github.com>
2024-12-05 11:27:43 -0800

    Passes Small Images Correctness
    Implemented pseudocode from ed

--- part2/conv2d.py
+++ part2/conv2d.py
@@ -33,46 +33,99 @@ out_pool_width = out_width // pool_size
 The shape of the output should be [batch_size, out_channels, out_pool_height, out_pool_width]
 
 """
-
 @nki.jit
 def fused_conv2d_maxpool(X, W, bias, pool_size=1):
 
-    batch_size, in_channels, input_height, input_width = X.shape
-    out_channels, in_channels_, filter_height, filter_width = W.shape
-    out_channels_ = bias.shape[0]
+   batch_size, in_channels, input_height, input_width = X.shape
+   out_channels, in_channels_, filter_height, filter_width = W.shape
+   out_channels_ = bias.shape[0]
 
-    assert (
-        in_channels_ == in_channels and out_channels_ == out_channels
-    ), f"Shape mismatch. {in_channels}, {in_channels_}, {out_channels}, {out_channels_}"
+   assert (
+       in_channels_ == in_channels and out_channels_ == out_channels
+   ), f"Shape mismatch. {in_channels}, {in_channels_}, {out_channels}, {out_channels_}"
 
-    out_height = input_height - filter_height + 1
-    out_width = input_width - filter_width + 1
+   out_height = input_height - filter_height + 1
+   out_width = input_width - filter_width + 1
 
-    out_pool_height = out_height // pool_size
-    out_pool_width = out_width // pool_size
-    
-    # Can assume multiple of 128 to avoid using mask
-    assert in_channels % 128 == 0
+   out_pool_height = out_height // pool_size
+   out_pool_width = out_width // pool_size
+   
+   # Can assume multiple of 128 to avoid using mask
+   assert in_channels % 128 == 0
 
-    # Can assume one PSUM bank can at least fit one row of the pixels
-    assert nl.tile_size.gemm_moving_fmax >= out_width
+   # Can assume one PSUM bank can at least fit one row of the pixels
+   assert nl.tile_size.gemm_moving_fmax >= out_width
 
-    # Initialize output array
-    X_out = nl.ndarray(
-        shape=(batch_size, out_channels, out_pool_height, out_pool_width),
-        dtype=X.dtype,
-        buffer=nl.hbm,
-    )
+   # Initialize output array
+   X_out = nl.ndarray(
+       shape=(batch_size, out_channels, out_pool_height, out_pool_width),
+       dtype=X.dtype,
+       buffer=nl.hbm,
+   )
 
-    # Various tiling dimensions (You may want to define more of them)
-    c_in_pmax = nl.tile_size.pmax
-    n_tiles_c_in = in_channels // c_in_pmax
+   # Various tiling dimensions (You may want to define more of them)
 
-    # Process the images in batches
-    for b in nl.affine_range(batch_size):
-        raise RuntimeError("Please fill your implementation of computing convolution"
-                           " of X[b] with the weights W and bias b, followed by a"
-                           " maxpool and store the result in X_out[b]")
+   c_in_pmax = 128
+   c_out_pmax = 128
 
-    return X_out
+   n_tiles_c_in = in_channels // 128
+   n_tiles_c_out = out_channels // 128 # because cout can't be too large so we need to divide in tiles
 
+   #preloading form
+   w_old = nl.ndarray(
+       shape=(n_tiles_c_out, nl.par_dim(c_out_pmax), n_tiles_c_in, 128, filter_height, filter_width),
+       dtype=W.dtype,
+       buffer=nl.sbuf
+   )
+
+   w_new = nl.ndarray(
+       shape=(filter_height, filter_width, n_tiles_c_out, n_tiles_c_in, nl.par_dim(c_out_pmax), c_in_pmax),
+       dtype=W.dtype,
+       buffer=nl.sbuf
+   )
+
+   for cout in nl.affine_range(n_tiles_c_out):
+        for cin in nl.affine_range(n_tiles_c_in):
+            w_old[cout, :, cin, :, :, :] = nl.load(W[cout * 128: cout * 128 + 128, cin * 128 : cin * 128 + 128, :, :])
+
+   # transpose part
+   for kh in nl.affine_range(filter_height):
+        for kw in nl.affine_range(filter_width):
+            for cout in nl.affine_range(n_tiles_c_out):
+                for cin in nl.affine_range(n_tiles_c_in):
+                    w_new[kh, kw, cout, cin] = nl.copy(w_old[cout, :, cin, :, kh, kw])
+                    
+   for img in nl.affine_range(batch_size):
+        #assign space in SBUF to store entire image, call it x
+        X_img = nl.ndarray(shape=(n_tiles_c_in, nl.par_dim(c_in_pmax), input_height, input_width), 
+            dtype=X[img].dtype, 
+            buffer=nl.sbuf)
+        
+        for cin in nl.affine_range(n_tiles_c_in):
+            X_img[cin, :, :, :] = nl.load(X[img, cin * 128: cin * 128 + 128, :, :])            
+        
+        for cout in nl.affine_range(n_tiles_c_out):
+            #asign space in SBUF to store output
+            output = nl.ndarray(shape=(nl.par_dim(c_out_pmax), out_height, out_width), 
+                dtype=X_out[img].dtype, 
+                buffer=nl.sbuf)
+
+            for out_row in nl.affine_range(out_height):
+                # assign space in PSUM to store output row
+                temp = nl.zeros((nl.par_dim(c_out_pmax), out_width), nl.float32, buffer=nl.psum)
+            
+                for kh in nl.affine_range(filter_height):
+                    for kw in nl.affine_range(filter_width):
+                        for cin in nl.affine_range(n_tiles_c_in):
+                            w_slice = w_new[kh, kw, cout, cin, :, :]
+                            x_slice = X_img[cin, :, out_row + kh, kw : kw + out_width]
+                            # Perform matrix multiplication and accumulate in PSUM
+                            temp += nl.matmul(w_slice, x_slice)
+                
+                #temp = nl.copy(temp, dtype=output[:, out_row, :].dtype)
+                output[:, out_row, :] = temp
+            
+            #output = nl.copy(output, dtype=X_out[img, cout * 128 : cout * 128 + 128, :, :].dtype)
+            nl.store(X_out[img, cout * 128 : cout * 128 + 128, :, :], value=output)
+
+   return X_out
